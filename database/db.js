9c5d4b8fa60c1c47b09b51a46be28e1666215f36
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-15 17:46:50 -0800

    linting cleanup

--- database/db.js
+++ database/db.js
@@ -6,20 +6,20 @@ const db = mongoose.connection;
 const housesSchema = new mongoose.Schema({
   price: Number,
   zipcode: String,
-  state: String
+  state: String,
 });
 const House = mongoose.model('House', housesSchema);
 
 const taxesSchema = new mongoose.Schema({
   state: String,
-  effective_tax_rate: Number
+  effective_tax_rate: Number,
 });
 const Tax = mongoose.model('Tax', taxesSchema);
 
 const loansSchema = new mongoose.Schema({
   type: String,
   years: Number,
-  interest_rate: Number
+  interest_rate: Number,
 });
 const Loan = mongoose.model('Loan', loansSchema);
 
@@ -27,5 +27,5 @@ module.exports = {
   connection: db,
   House,
   Tax,
-  Loan
+  Loan,
 };
