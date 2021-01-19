ac3df876a2935a88263ad2ad83cc042f67a3c911
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-19 12:30:59 -0800

    Move all styles into global styles styled components. delete .css file

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import TableStyles from '../global_styles/TableStyles.jsx';
 
 const Form = () => (
-  <div id="form">
-    <table className="form-table">
-      <tbody>
-        <tr>
-          <td>
-            <h2>Home Price</h2>
-          </td>
-          <td>
-            <h2>Down Payment</h2>
-          </td>
-          <td>
-            <h2>Interest Rate</h2>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h2>Loan Type</h2>
-          </td>
-          <td />
-          <td />
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <>
+    <TableStyles />
+    <div id="form">
+      <table className="form-table">
+        <tbody>
+          <tr>
+            <td>
+              <h2>Home Price</h2>
+            </td>
+            <td>
+              <h2>Down Payment</h2>
+            </td>
+            <td>
+              <h2>Interest Rate</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>Loan Type</h2>
+            </td>
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </>
 );
 
 export default Form;
---
+++ client/global_styles/TableStyles.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const TableStyles = createGlobalStyle`
+  .form-table {
+    width: 100%;
+  }
+
+  .form-table td {
+    width: 33%;
+  }
+
+`;
+
+export default TableStyles;
