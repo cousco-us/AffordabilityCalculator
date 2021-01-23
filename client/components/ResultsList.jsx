3158cbdd14024de7206b2ddfbbe319b83ad33c20
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-23 14:01:06 -0800

    Button hover styles

--- client/components/ResultsList.jsx
+++ client/components/ResultsList.jsx
@@ -30,6 +30,12 @@ const Button = styled.button`
   color: rgb(255, 255, 255);
   background-color: rgb(0, 120, 130);
   border-color: transparent;
+
+  &:hover {
+    color: rgb(0, 120, 130);
+    background-color: rgb(255, 255, 255);
+    border-color: rgb(0, 120, 130);
+  }
 `;
 
 const ResultsList = ({
