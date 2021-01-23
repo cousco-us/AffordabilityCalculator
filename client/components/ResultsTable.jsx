054890dc9106ddcb81a0e4ea2a9e42a708561bc9
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-22 22:05:08 -0800

    Fix console.log bug in money helper. Re-format zero dollar values in results table

--- client/components/ResultsTable.jsx
+++ client/components/ResultsTable.jsx
@@ -28,7 +28,7 @@ const ResultsTable = ({ donutData }) => (
             {title}
           </div>
           <div className="table-col money">
-            {moneyValue ? Money.formatMoney(moneyValue) : moneyValue}
+            {moneyValue ? Money.formatMoney(moneyValue) : `$${moneyValue}`}
 
           </div>
         </div>
