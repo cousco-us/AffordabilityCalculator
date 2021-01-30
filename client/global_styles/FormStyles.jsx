a0ce494befd11ea1c867218d99db271e0678b569
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-30 11:33:15 -0800

    tweak slider styles

--- client/global_styles/FormStyles.jsx
+++ client/global_styles/FormStyles.jsx
@@ -44,13 +44,16 @@ const FormStyles = styled.div`
     margin-top: -7px;
   }
 
-  input {
+  input[type=text] {
+    @import url(https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap);
+    font-family: 'Cabin', Arial, Helvetica, sans-serif;
     letter-spacing: -0.1px;
     font-size: 16px;
     line-height: 1.5;
     padding: 6px;
     border-radius: 6px;
     border: solid 0.01em rgb(205, 209, 212);
+    color: rgb(59, 65, 68);
   }
 
   #input-control-home-price-input {
@@ -89,35 +92,31 @@ const FormStyles = styled.div`
     padding: 12px 4px;
   }
 
+  input[type=range] {
+    width: 100%;
+    height: 2px;
+    appearance: none;
+    -webkit-appearance: none;
+    outline: none;
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  input[type=range]::-webkit-slider-runnable-track {
+    background: rgb(48, 118, 128);
+    height: 2px;
+    }
+
   input[type=range]::-webkit-slider-thumb {
     -webkit-appearance: none;
     border: solid 4px white;
     height: 20px;
     width: 20px;
     border-radius: 50%;
-    background: rgb(48, 118, 148);
+    background: rgb(48, 118, 128);
     filter: drop-shadow(0.02em 0.08em 0.08em black);
-  }
-
-  input[type=text] {
-    @import url(https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap);
-    font-family: 'Cabin', Arial, Helvetica, sans-serif;
-    color: rgb(59, 65, 68);
-    letter-spacing: -0.1px;
-  }
-
-  .calculator-slider {
-    width: 100%;
-    height: 2px;
-    appearance: none;
-    outline: none;
-    margin: 0px;
-    padding: 0px;
-    transition: background 450ms ease-in;
-    -webkit-appearance: none;
-
-    outline: none;
-    box-sizing: border-box;
+    margin-top: -10px;
   }
 
   @media only screen and (max-width: 750px) {
